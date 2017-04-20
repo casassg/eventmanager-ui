7b7a127a19aebd2707802205fa7404e4f1f2c439
Gerard Casas Saez <casassg@gmail.com>
2017-04-20 13:23:10 -0600

    fix signal kafka send problem

--- events/signals.py
+++ events/signals.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from functools import reduce
 
 from django.conf import settings
@@ -16,38 +17,24 @@ UPDATE_ACTION = 'update'
 IGNORE_ACTION = 'ignore'
 
 
-def create_message(type, action, data=None):
-    return {
-        'type': type,
-        'action': action,
-        'data': data
-    }
-
-
 @receiver(pre_save, sender=models.Event)
 def kakfa_check(sender, instance, **kwargs):
-    producer = KafkaProducer(
-        bootstrap_servers=settings.KAFKA_SERVERS,
-        retries=5,
-        value_serializer=lambda v: json.dumps(v).encode('utf-8')
-    )
+    producer = get_producer()
     message = create_message(TEST_TYPE, 'ignore')
     try:
-        producer.send('events', message)
+        logging.info('Checking connexion...')
+        producer.send(settings.KAFKA_TOPIC, message)
     except:
         client = SimpleClient(hosts=settings.KAFKA_SERVERS)
         client.ensure_topic_exists(settings.KAFKA_TOPIC)
         client.close()
         producer.send(settings.KAFKA_TOPIC, message)
+    producer.close(10)
 
 
 @receiver(post_save, sender=models.Event)
 def kakfa_event_update(sender, instance, **kwargs):
-    producer = KafkaProducer(
-        bootstrap_servers=settings.KAFKA_SERVERS,
-        retries=5,
-        value_serializer=lambda v: json.dumps(v).encode('utf-8')
-    )
+    producer = get_producer()
     message = create_message(EVENT_TYPE, 'update', instance.to_dict())
     try:
         producer.send(settings.KAFKA_TOPIC, message)
@@ -56,15 +43,12 @@ def kakfa_event_update(sender, instance, **kwargs):
         client.ensure_topic_exists(settings.KAFKA_TOPIC)
         client.close()
         producer.send(settings.KAFKA_TOPIC, message)
+    producer.close(10)
 
 
 @receiver(post_save, sender=models.Event)
 def kakfa_queries_check(sender, instance, **kwargs):
-    producer = KafkaProducer(
-        bootstrap_servers=settings.KAFKA_SERVERS,
-        retries=5,
-        value_serializer=lambda v: json.dumps(v).encode('utf-8')
-    )
+    producer = get_producer()
     query = models.TwitterOutQuery.objects.first()
     new_keywords = get_new_keywords()
     if query.queries == new_keywords:
@@ -79,6 +63,24 @@ def kakfa_queries_check(sender, instance, **kwargs):
         client.ensure_topic_exists(settings.KAFKA_TOPIC)
         client.close()
         producer.send(settings.KAFKA_TOPIC, message)
+    producer.close(10)
+
+
+def create_message(type, action, data=None):
+    return {
+        'type': type,
+        'action': action,
+        'data': data
+    }
+
+
+def get_producer():
+    producer = KafkaProducer(
+        bootstrap_servers=settings.KAFKA_SERVERS,
+        retries=5,
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+    return producer
 
 
 def get_new_keywords():
